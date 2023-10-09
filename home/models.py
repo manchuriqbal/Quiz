ab011c5bba222aef4faa117c107ff138336419da
manchuriqbal <manchur.fiverr@gmail.com>
2023-10-09 16:21:41 +0600

    inisial and add some questions and answer

---
+++ home/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from uuid import uuid4
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    category_name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.category_name
+    
+class Question(BaseModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='questions')
+    question_text = models.CharField(max_length=100)
+    marks = models.IntegerField(default=5)
+
+    def __str__(self) -> str:
+        return self.question_text
+    
+class Answer(BaseModel):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    answer_text = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.answer_text
