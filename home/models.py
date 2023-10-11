bd5b925733cd2f5ebfbe2d955cd7ba0df58700c9
manchuriqbal <manchur.fiverr@gmail.com>
2023-10-11 13:35:01 +0600

    i complate this quiz project

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+import random
 
 class BaseModel(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -24,6 +25,19 @@ class Question(BaseModel):
     def __str__(self) -> str:
         return self.question_text
     
+    def get_answer(self):
+        answer_objs = list(Answer.objects.filter(question = self))
+        random.shuffle(answer_objs)
+        data = []
+
+        for answer_obj in answer_objs:
+            data.append({
+                'answer': answer_obj.answer_text,
+                'is_correct' : answer_obj.is_correct
+            })
+
+        return data
+    
 class Answer(BaseModel):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
     answer_text = models.CharField(max_length=100)
@@ -31,3 +45,7 @@ class Answer(BaseModel):
 
     def __str__(self) -> str:
         return self.answer_text
+    
+
+
+
--- home/views.py
+++ home/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+import random
+from .models import *
 
 def home(request):
-    return HttpResponse('Hello this is website!')
+    context = {'categories': Category.objects.all()}
+    if request.GET.get('category'):
+        return redirect(f"quiz/?category={request.GET.get('category')}")
+    return render(request, 'home.html', context)
+
+def quiz(request):
+    context = {'category' : request.GET.get('category')}
+    return render(request, 'quiz.html', context)
+
+def get_quiz(request):
+    try:
+        question_obj =Question.objects.all()
+        if request.GET.get('category'):
+            question_obj = question_obj.filter(category__category_name__icontains=request.GET.get('category'))
+        question_obj =list(question_obj)
+        data = []
+        random.shuffle(question_obj)
+        for question in question_obj:
+            data.append({
+                'uuid' : question.uuid,
+                'category': question.category.category_name,
+                'question' : question.question_text,
+                'marks': question.marks,
+                'answer': question.get_answer()
+            })
+        payload = {'status': True, 'data': data}
+
+        return JsonResponse(payload)
+
+    except Exception as e:
+        print(e)
